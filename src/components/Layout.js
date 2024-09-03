a58a9946473c1b846e39a76b78392fa230958dc2
sanki92 <sankalpt92@gmail.com>
2024-09-04 00:47:14 +0530

    feat: integrate LanguageToggle button in Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
-
+import Preloader from './Preloader'
+import LanguageToggle from './LanguageToggle'
 const Layout = ({children}) => {
+  
   return (
     <div>
+      <Preloader/>
         <Navbar/>
         {children}
+        {/* <div className='fixed bottom-10 z-30 left-0 w-[10rem]'>
+          <LanguageToggle/>
+        </div> */}
         <Footer/>
     </div>
   )
