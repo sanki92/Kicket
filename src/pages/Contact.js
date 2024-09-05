b31921bee7f444c65c4e8485de5a7d38228b9bc4
sanki92 <sankalpt92@gmail.com>
2024-09-05 19:55:04 +0530

    feat(Contact): integrate api for dynamic content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,34 +8,53 @@ import ourService1phone from "../assets/our-service-phone.png";
 
 import Experience from "../components/Experience";
 import axiosConfig from "../axios/axiosConfig";
+import { useLanguage } from "../components/LanguageContext";
 const Contact = () => {
   const [isPhone, setIsPhone] = useState(false);
-  const [formData, setFormData]= useState({
-    name:"",
-    email:"",
-    message:""
-  })
-
-  function handleOnChange(e){
-    const {name, value} = e.target;
-    setFormData({...formData, [name]:value});
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { language } = useLanguage();
+  const [content, setContent] = useState({
+    en: "",
+    ar: "",
+  });
+  function handleOnChange(e) {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   }
 
-  function handleSubmit(){
+  const fetchContent = async () => {
     try {
-      const res = axiosConfig.post('/api/contact',formData);
-      setFormData({
-        name:"",
-        email:"",
-        message:""
-      })
-      console.log(res)
+      const response = await axiosConfig.get("/api/contact-page-content");
+      console.log("contact", response);
+      setContent({
+        en: response.data.form_title,
+        ar: response.data.arabic_form_title,
+      });
     } catch (error) {
-      console.error(error)
+      console.error("Error fetching home content data:", error);
+    }
+  };
+
+  function handleSubmit() {
+    try {
+      const res = axiosConfig.post("/api/contact", formData);
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+      console.log(res);
+    } catch (error) {
+      console.error(error);
     }
   }
 
   useEffect(() => {
+    fetchContent();
     const handleResize = () => {
       setIsPhone(window.innerWidth <= 767);
     };
@@ -59,8 +78,11 @@ const Contact = () => {
         <img src={ourService1phone} className="z-10 hidden smM:block" />
 
         <div className="flex flex-col justify-center gap-y-5">
-          <h1 className="text-white font-[750] text-[45px] smM:text-[7vw] smM:whitespace-nowrap smM:mt-5 smM:text-center">
-            Get In Touch With Us
+          <h1
+            dir={language === "ar" ? "rtl" : "ltr"}
+            className="text-white font-[750] text-[45px] smM:text-[7vw] smM:whitespace-nowrap smM:mt-5 smM:text-center"
+          >
+            {content[language]}
           </h1>
           <input
             placeholder="Name"
@@ -83,7 +105,10 @@ const Contact = () => {
             className="smM:text-[4vw] h-[8rem] py-2 px-4 rounded-3xl border bg-transparent"
             type="text"
           />
-          <button onClick={handleSubmit} className=" w-fit mt-5 smM:py-2 smM:px-[2rem] smM:text-[3.5vw] smM:leading-[6vw] py-4 px-[3rem] rounded-[50px] text-xl bg-gradient-to-r from-[#ED4C75] to-[#973EFF] text-white">
+          <button
+            onClick={handleSubmit}
+            className=" w-fit mt-5 smM:py-2 smM:px-[2rem] smM:text-[3.5vw] smM:leading-[6vw] py-4 px-[3rem] rounded-[50px] text-xl bg-gradient-to-r from-[#ED4C75] to-[#973EFF] text-white"
+          >
             Submit{" "}
           </button>
         </div>
