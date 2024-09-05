aa6696ceb2d7a726dac3a2f8e9bb5b3462f84ca4
sanki92 <sankalpt92@gmail.com>
2024-09-05 19:56:21 +0530

    feat: integrate api for dynamic content and added translation support

--- src/pages/Service1.js
+++ src/pages/Service1.js
@@ -7,11 +7,82 @@ import Stories from "../components/WhyKicket/Stories";
 import ourService1phone from "../assets/our-service-phone.png";
 import Experience from "../components/Experience";
 import Slider from "react-slick";
+import axiosConfig from "../axios/axiosConfig";
+import { useLanguage } from "../components/LanguageContext";
 
 const Service1 = () => {
   const [isPhone, setIsPhone] = useState(false);
+  const [data, setData] = useState({
+    main_image: "",
+    en: {
+      title: "",
+      description: "",
+    },
+    ar: {
+      title: "",
+      description: "",
+    },
+  });
+  const [features, setFeatures] = useState([
+    {
+      image: "",
+      number: "",
+      en: {
+        title: "",
+        description: "",
+      },
+      ar: {
+        title: "",
+        description: "",
+      },
+    },
+  ]);
+  const { language } = useLanguage();
+
+  const fetchService = async () => {
+    try {
+      const response = await axiosConfig.get("/api/service/1");
+      console.log("service 1", response);
+      setData({
+        main_image: response.data.main_image,
+        en: {
+          title: response.data.title,
+          description: response.data.description,
+        },
+        ar: {
+          title: response.data.arabic_title,
+          description: response.data.arabic_description,
+        },
+      });
+    } catch (error) {
+      console.error("Error fetching home content data:", error);
+    }
+  };
+  const fetchFeatures = async () => {
+    try {
+      const response = await axiosConfig.get("/api/features/1");
+      console.log("service 1 keyfeature", response);
+      const data = response.data.map((e) => ({
+        image: "",
+        number: "",
+        en: {
+          title: "",
+          description: "",
+        },
+        ar: {
+          title: "",
+          description: "",
+        },
+      }));
+      setFeatures();
+    } catch (error) {
+      console.error("Error fetching home content data:", error);
+    }
+  };
 
   useEffect(() => {
+    fetchFeatures();
+    fetchService();
     const handleResize = () => {
       setIsPhone(window.innerWidth <= 767);
     };
@@ -30,7 +101,7 @@ const Service1 = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    arrows:false,
+    arrows: false,
     responsive: [
       {
         breakpoint: 1024,
@@ -49,38 +120,38 @@ const Service1 = () => {
     ],
   };
 
-  const features = [
-    {
-      number: 1,
-      title: "Service 1",
-      description: "Description for service 1.",
-    },
-    {
-      number: 2,
-      title: "Service 2",
-      description: "Description for service 2.",
-    },
-    {
-      number: 3,
-      title: "Service 3",
-      description: "Description for service 3.",
-    },
-    {
-      number: 4,
-      title: "Service 4",
-      description: "Description for service 4.",
-    },
-    {
-      number: 5,
-      title: "Service 5",
-      description: "Description for service 5.",
-    },
-    {
-      number: 6,
-      title: "Service 6",
-      description: "Description for service 6.",
-    },
-  ];
+  // const features = [
+  //   {
+  //     number: 1,
+  //     title: "Service 1",
+  //     description: "Description for service 1.",
+  //   },
+  //   {
+  //     number: 2,
+  //     title: "Service 2",
+  //     description: "Description for service 2.",
+  //   },
+  //   {
+  //     number: 3,
+  //     title: "Service 3",
+  //     description: "Description for service 3.",
+  //   },
+  //   {
+  //     number: 4,
+  //     title: "Service 4",
+  //     description: "Description for service 4.",
+  //   },
+  //   {
+  //     number: 5,
+  //     title: "Service 5",
+  //     description: "Description for service 5.",
+  //   },
+  //   {
+  //     number: 6,
+  //     title: "Service 6",
+  //     description: "Description for service 6.",
+  //   },
+  // ];
 
   return (
     <Layout>
@@ -88,26 +159,31 @@ const Service1 = () => {
         className="bg-cover bg-center h-[60vh] flex justify-center items-center relative"
         style={{ backgroundImage: `url(${bgImage})` }}
       >
-        <h1 className="text-[#FFC019] font-[750] text-[45px]">Service 1</h1>
+        <h1
+          dir={language === "ar" ? "rtl" : "ltr"}
+          className="text-[#FFC019] font-[750] text-[45px]"
+        >
+          {data[language].title}
+        </h1>
         <div className="w-full h-[10rem] bg-gradient-to-b from-black/0 to-[#252525]/100 absolute bottom-0"></div>
       </div>
 
       <div className="flex lgM:flex-col lgM:items-center text-white text-[20px] w-[85%] mx-auto gap-x-14 mt-14">
-        <img src={aboutImg} className="smM:hidden w-[30rem]" />
+        <img
+          src={`https://kicketapi.webprismits.us/assets/services/${data[language].main_image}`}
+          className="smM:hidden w-[30rem]"
+        />
         <img src={ourService1phone} className="z-10 hidden smM:block" />
 
         <div className="flex flex-col justify-center gap-y-5">
           <h1 className="text-white font-[750] text-[45px] lgM:text-center smM:text-[7vw] smM:whitespace-nowrap smM:mt-5 smM:text-center">
             Service
           </h1>
-          <p className="leading-[35px] smM:text-[4vw] smM:leading-[6vw]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+          <p
+            dir={language === "ar" ? "rtl" : "ltr"}
+            className="leading-[35px] smM:text-[4vw] smM:leading-[6vw]"
+          >
+            {data[language].description}
           </p>
         </div>
       </div>
@@ -120,10 +196,7 @@ const Service1 = () => {
           {features.map((feature) => (
             <>
               {feature.number % 2 === 1 ? (
-                <div
-                  key={feature.number}
-                  className="text-white"
-                >
+                <div key={feature.number} className="text-white">
                   <h2 className="text-[25px] text-center">{feature.title}</h2>
                   <p className="text-[18px] text-[#D9D9D9] text-center">
                     {feature.description}
@@ -132,7 +205,7 @@ const Service1 = () => {
                     {feature.number}
                     <div className="absolute right-0 top-0 h-full w-[4px] bg-gradient-to-b from-[#ffc019] via-[#ed4c75] via-[#973eff] to-[#00d6cc]"></div>
                   </h1>
-                  <hr className="my-5 mt-[3.99rem]"/>
+                  <hr className="my-5 mt-[3.99rem]" />
                   <div className="flex justify-center">
                     <div className="bg-[#363637] border border-[#D9D9D9] h-[15rem] w-[18rem] rounded-2xl flex items-center justify-center">
                       <img src={ticket} alt={`ticket ${feature.number}`} />
@@ -140,16 +213,13 @@ const Service1 = () => {
                   </div>
                 </div>
               ) : (
-                <div
-                  key={feature.number}
-                  className="text-white space-y-8"
-                >
+                <div key={feature.number} className="text-white space-y-8">
                   <div className="flex justify-center">
                     <div className="bg-[#363637] border border-[#D9D9D9] h-[15rem] w-[18rem] rounded-2xl flex items-center justify-center">
                       <img src={ticket} alt={`ticket ${feature.number}`} />
                     </div>
                   </div>
-                  <hr/>
+                  <hr />
 
                   <h1 className="relative w-fit leading-[144px] px-14 text-[120px] text-[#4E4F4F] font-[750] mx-auto">
                     {feature.number}
--- src/pages/WhyKicket.js
+++ src/pages/WhyKicket.js
@@ -4,21 +4,97 @@ import aboutImg from "../assets/whyKicket/aboutImg.png";
 import Stories from "../components/WhyKicket/Stories";
 import WhyChoose from "../components/WhyKicket/WhyChoose";
 import Experience from "../components/Experience";
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
 import Footer from "../components/Footer";
 import ourService1phone from "../assets/our-service-phone.png";
 import axiosConfig from "../axios/axiosConfig";
+import { useLanguage } from "../components/LanguageContext";
 
 const WhyKicket = () => {
   const [isPhone, setIsPhone] = useState(false);
-  const [stories, setStories] = useState([]);
-
-
+  const [stories, setStories] = useState([
+    {
+      pdf:"",
+      image: "",
+      en: {
+        title: "",
+        description: "",
+      },
+      ar: {
+        title: "",
+        description: "",
+      },
+    },
+  ]);
+  const [whyChooseUs, setWhyChooseUs] = useState([
+    {
+      en: {
+        title: "",
+        description: "",
+      },
+      ar: {
+        title: "",
+        description: "",
+      },
+    },
+  ]);
+  const [about, setAbout] = useState({
+    image:"",
+    en: "",
+    ar: "",
+  });
+  const { language } = useLanguage();
   const fetchStories = async () => {
     try {
       const response = await axiosConfig.get("/api/success-stories");
-      console.log("stories data:", response.data); // Debugging line
-      setStories(response.data);
+
+      const data = response.data.map((e) => ({
+        image: e.image,
+        pdf:e.file,
+        en: {
+          title: e.title,
+          description: e.description,
+        },
+        ar: {
+          title: e.arabic_title,
+          description: e.arabic_description,
+        },
+      }));
+      console.log("**** success", data);
+      setStories(data);
+    } catch (error) {
+      console.error("Error fetching home content data:", error);
+    }
+  };
+
+  const fetchWhyChooseUs = async () => {
+    try {
+      const response = await axiosConfig.get("/api/selling-points");
+      const data = response.data.map((e) => ({
+        en: {
+          title: e.title,
+          description: e.description,
+        },
+        ar: {
+          title: e.arabic_title,
+          description: e.arabic_description,
+        },
+      }));
+      setWhyChooseUs(data);
+    } catch (error) {
+      console.error("Error fetching home content data:", error);
+    }
+  };
+
+  const fetchAbout = async () => {
+    try {
+      const response = await axiosConfig.get("/api/about-content");
+      setAbout({
+        image:response.data[0].image,
+        en: response.data[0].description,
+        ar: response.data[0].arabic_description,
+      });
+      console.log("about data:", response.data); // Debugging line
     } catch (error) {
       console.error("Error fetching home content data:", error);
     }
@@ -26,6 +102,8 @@ const WhyKicket = () => {
 
   useEffect(() => {
     fetchStories();
+    fetchWhyChooseUs();
+    fetchAbout();
     const handleResize = () => {
       setIsPhone(window.innerWidth <= 767);
     };
@@ -47,34 +125,34 @@ const WhyKicket = () => {
         </div>
 
         <div className=" flex lgM:flex-col lgM:items-center text-white  text-[20px] w-[85%] smM:w-[90%] mx-auto gap-x-14 mt-14">
-          <img src={aboutImg} className="smM:hidden w-[30rem]" />
+          <img src={`https://kicketapi.webprismits.us/assets/about/${about.image}`} className="smM:hidden w-[30rem]" />
           <img src={ourService1phone} className="hidden smM:block" />
 
-
           <div className="flex flex-col justify-center gap-y-5 lgM:items-center">
-            <h1 className="text-white font-[750] text-[45px] smM:text-[7vw] smM:whitespace-nowrap smM:text-center smM:mt-3">About Kicket</h1>
-            <p className="leading-[35px]  smM:text-[4vw] smM:leading-[6vw] lgM:text-[2vw] lgM:leading-[3vw]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <h1 className="text-white font-[750] text-[45px] smM:text-[7vw] smM:whitespace-nowrap smM:text-center smM:mt-3">
+              About Kicket
+            </h1>
+            <p
+              dir={language === "ar" ? "rtl" : "ltr"}
+              className="leading-[35px]  smM:text-[4vw] smM:leading-[6vw] lgM:text-[2vw] lgM:leading-[3vw]"
+            >
+              {about[language]}
             </p>
           </div>
         </div>
 
-        <Stories data={stories} dir={"stories"} isPhone={isPhone}/>
+        <Stories data={stories} dir={"stories"} isPhone={isPhone} />
 
         <div className=" flex lgM:flex-col lgM:items-center text-white text-[20px] w-[85%] smM:w-[90%] mx-auto gap-x-14 mt-14">
           <img src={aboutImg} className="smM:hidden w-[30rem]" />
           <img src={ourService1phone} className="z-10 hidden smM:block" />
 
           <div className="flex flex-col justify-center gap-y-10">
-            <h1 className="text-white font-[750] lgM:text-center text-[45px] smM:text-[7vw] smM:whitespace-nowrap smM:text-center smM:mt-5">Why Choose Us</h1>
+            <h1 className="text-white font-[750] lgM:text-center text-[45px] smM:text-[7vw] smM:whitespace-nowrap smM:text-center smM:mt-5">
+              Why Choose Us
+            </h1>
 
-            <WhyChoose />
+            <WhyChoose data={whyChooseUs} />
           </div>
         </div>
 
